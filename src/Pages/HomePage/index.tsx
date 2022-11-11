f478488c3e4002fb8f0f2d70eaec23a164879da2
Roger David Alba Ortega <99060136+Davidalbort@users.noreply.github.com>
2022-11-10 23:57:47 -0500

    Styles(Layout): Add component Layout

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,20 +1,11 @@
-import { AllCounter } from "../../components/AllCounter";
-import { AllItems } from "../../components/AllItems";
-import { AllSearch } from "../../components/AllSearch";
-
-
-
+import { Layout } from "../Layout";
 
 const HomePage = () => {
   return(
     <>
       <h1>HomePage</h1>
-      <AllCounter 
-        totalAll={4}
-        completedAll={2}
-      />
-      <AllSearch />
-      <AllItems />
+      <Layout />
+      
     </>
   )
 }
